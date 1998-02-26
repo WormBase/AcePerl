b73d1ba83a053571a6b9d21db012405ccd1650bd
lstein <lstein>
1998-02-26 20:49:05 +0000

    This was committed just before the big reorganization.

--- test2.pl
+++ test2.pl
@@ -3,27 +3,10 @@
 use lib './lib';
 use Ace;
 
-$db = Ace->connect(-host=>'sapiens',-port=>123456,-path=>'~mieg/bin/aceclient');
+$db = Ace->connect(-host=>'localhost',-port=>200001,-path=>'~acedb/bin/aceclient');
+@sequences = $db->list('Sequence','m*');
 
-my $ace = $db->fetch('Sequence','DBEST:20415');
-print $ace->asString,"\n";
-
-my $dna = $ace->DNA->pick;
-print $dna->asString;
-
-print "Identifier is ",join(' ',$ace->at('DB_info.identifier')->row),"\n";
-print "Title is ", $ace->Title,"\n";
-
-@h = $ace->DNA_homol;
-print "First homology is $h[0]\n";
-
-print "Clone is ",$ace->Clone,"\n More info:\n";
-$clone=$ace->Clone->pick;
-print $clone->asString;
-
-my @objects = $db->list('Sequence','DBEST:2963*');
-foreach (@objects) {
-    print "$_: ",$_->Title,"\n";
+foreach (@sequences) {
+  @r = $_->tags;
+  print scalar(@r),"\n";
 }
-print scalar(@objects)," objects\n";
-
