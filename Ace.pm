aeff6ad6eeaab2f938da1389c142cf91dc855e33
lstein <lstein>
2001-07-20 19:40:09 +0000

    this is version 1.75

--- Ace.pm
+++ Ace.pm
@@ -17,7 +17,7 @@ use overload
 
 # Optional exports
 @EXPORT_OK = qw(rearrange ACE_PARSE);
-$VERSION = '1.74';
+$VERSION = '1.75';
 
 use constant STATUS_WAITING => 0;
 use constant STATUS_PENDING => 1;
