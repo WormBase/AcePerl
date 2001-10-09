b312a9cf3f0483870db999e4aaef51334e231c50
lstein <lstein>
2001-10-09 15:00:33 +0000

    fixed makefile.pl

--- Ace.pm
+++ Ace.pm
@@ -17,7 +17,7 @@ use overload
 
 # Optional exports
 @EXPORT_OK = qw(rearrange ACE_PARSE);
-$VERSION = '1.77';
+$VERSION = '1.79';
 
 use constant STATUS_WAITING => 0;
 use constant STATUS_PENDING => 1;
