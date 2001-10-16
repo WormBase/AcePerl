33e7c1e2a5e0f302a887e97f2cae6a20d2c834aa
lstein <lstein>
2001-10-16 21:41:43 +0000

    fixed change log

--- Ace.pm
+++ Ace.pm
@@ -17,7 +17,7 @@ use overload
 
 # Optional exports
 @EXPORT_OK = qw(rearrange ACE_PARSE);
-$VERSION = '1.79';
+$VERSION = '1.80';
 
 use constant STATUS_WAITING => 0;
 use constant STATUS_PENDING => 1;
