5cf391bd452d2763536e2cd920e5ae2afffa8f39
lstein <lstein>
2008-11-11 16:44:48 +0000

    bumped to version 1.92; fixed rare problem caching broken objects without name

--- Ace.pm
+++ Ace.pm
@@ -20,7 +20,7 @@ use overload
 
 # Optional exports
 @EXPORT_OK = qw(rearrange ACE_PARSE);
-$VERSION = '1.91';
+$VERSION = '1.92';
 
 use constant STATUS_WAITING => 0;
 use constant STATUS_PENDING => 1;
